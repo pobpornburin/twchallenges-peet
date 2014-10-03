db0e378696cc63d3d039b8ef0c5ab8c4f202e670
pobporn burin <pobporn.b@taskworld.com>
2014-10-03 14:00:11 +0700

    adjust HTML & CSS file

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 // var stylus = require('stylus');
 // var nib = require('nib');
 
-//var routes = require('./routes/index');
-//var users = require('./routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -25,25 +25,21 @@ app.set('view engine', 'jade');
 //   , compile: compile
 //   }
 // ))
-//app.engine('html', require('hogan-express'));
-//app.set('view engine', 'html');
-//app.set('layout', 'layouts/default');
-//app.set('partials', {header: "includes/header"});
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-//app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req,res) {
     res.render("index", {title: "Tw-challenges #1 Pobpornburin"});
 })
 
-//app.use('/', routes);
-//app.use('/users', users);
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
